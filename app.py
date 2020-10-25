ba05b8ef6cfb7837dae4cd7b8a10398076f1f209
shahrour <ahmadshahrour951@gmail.com>
2020-10-25 15:06:31 -0700

    Initiate Flask

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+from flask import Flask
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(debug=True)

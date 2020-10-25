9570bc6d5cf1e21ec719ad0fd8978ae3a0a98a21
shahrour <ahmadshahrour951@gmail.com>
2020-10-25 15:07:31 -0700

    Create adhoc animal URLs

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ app = Flask(__name__)
 def homepage():
     """Shows a greeting to the user."""
     return 'Are you there, world? It\'s me, Ducky!'
+
+
+@app.route('/penguins')
+def favanimalpage():
+    """Shows a message about peguins"""
+    return "Penguins are cute!"
+
+
+@app.route('/peregrine')
+def myfavanimalpage():
+    """Shows a message about the peregrine falcon"""
+    return "Peregrine falcon's are wayyyy faster than you!"
+
+
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
     app.run(debug=True)
